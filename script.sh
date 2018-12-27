edc0b194f9bd5b148c0a5976232a423edb1b47cb
Karol-Szykula <karol.szykula@wp.pl>
2018-12-28 00:19:00 +0100

    Example of testing first option from shell

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 
-if [ "foo" = "foo" ]; then
-               echo expression evaluated as tru
-       fi
+if [ "$1" = witaj ];then
+	echo 'This is first test' 
+fi
+if [ x"$1" = x"witaj" ]; then
+	echo 'this is second test'
+fi
